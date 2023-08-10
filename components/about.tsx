815e35250ebc4caf3ddcdc4c0d6e13cf44f4cee6
Ritik Giri <130232028+Ritikk-sudo@users.noreply.github.com>
2023-08-10 22:19:21 +0530

    clean up site data's

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,29 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        After passing 12th, I decided to pursue my
+        passion for programming and as well as 'BCA' Degree. I enrolled in coding and pursuing 'BCA' and focusing on my {" "}
+        <span className="font-medium">Full-Stack Web Development</span> Skills.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+        <span className="font-medium text-blue-400">
+          React, Next.js, Node.js, MongoDB, Express JS and Tailwind CSS
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        . I am also familiar with <span className="text-blue-400">TypeScript</span>. I am always looking to
+        learn new Technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a Software
+        Developer.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, watching movies, and listening to music. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">more about React and JavaScript</span>. I'm also
+        learning Video Editing.
       </p>
     </motion.section>
   );
